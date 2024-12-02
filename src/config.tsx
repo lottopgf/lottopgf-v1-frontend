6f2daba1720a4e4f36154f11d77676870f2da31d
maggo <me@mar.co.de>
2024-12-02 16:19:24 +0700

    feat: migrate to base example deployment

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,6 @@
 import { APP_URL } from "@/lib/host";
 import { type Address } from "viem";
-import { scroll } from "viem/chains";
+import { base, scroll } from "viem/chains";
 
 // Project metadata
 export const METADATA = {
@@ -20,7 +20,7 @@ export const METADATA = {
 };
 
 // The chain where your lottery contract is deployed
-export const CHAIN = scroll;
+export const CHAIN = base;
 
 // The ticker of the prize token. Will be used when rendering prizes.
 export const PRIZE_TOKEN_TICKER = "ETH";
@@ -31,17 +31,23 @@ export const PRIZE_TOKEN_IS_NATIVE = true;
 
 // The contract address of the lottery
 export const CONTRACT_ADDRESS: Address =
-  "0xD14E428C78C9dA280028C462aA3E5D2e3105497B";
+  "0xF7d114B2F637fE7Cf7aD659BbA43B1FE64D815eC";
 
 // The address of the ETH adapter contract
 export const LOOTERY_ETH_ADAPTER_ADDRESS: Address =
-  "0x51A60D80Fa6d5FEDeb87E615Ed1D41661CB42A69";
+  "0xF9aC611be31983EB9030d2F201Af566b59d5BCE2";
 
 // The URL of the GraphQL API to get ticket data
 export const GRAPHQL_API =
-  "https://zuzalotto-indexer-production.up.railway.app";
+  "https://lootery-v1-indexer-base-prod.up.railway.app";
 
 // The amount of money you're trying to raise.
 // It will show a progress bar inside of the "funds raised" card.
 // Set to null to disable the progress bar
 export const FUNDRAISE_TARGET: bigint | null = null;
+
+// Maps chain ids to the respective name on relay.link
+export const RELAY_BRIDGE_NAME_MAPPING = {
+  [scroll.id]: "scroll",
+  [base.id]: "base",
+};
